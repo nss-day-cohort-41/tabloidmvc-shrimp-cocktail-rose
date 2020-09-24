72c7d64535f645da8a37d1d4996048436bc09558
davidbrucetn <davidbrucetn@gmail.com>
2020-09-24 14:35:01 -0500

    Add Tag work

---
+++ TabloidMVC/Controllers/PostTagController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using TabloidMVC.Models;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class PostTagController : Controller
+    {
+
+        private readonly IPostRepository _postRepository;
+        private readonly ITagRepository _tagRepository;
+
+        public PostTagController(IPostRepository postRepository, ITagRepository tagRepository)
+        {
+            _postRepository = postRepository;
+            _tagRepository = tagRepository;
+        }
+        // GET: PostTagController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PostTagController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PostTagController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostTagController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostTagController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Post post = _postRepository.GetPublishedPostById(id);
+            List<Tag> postTags = _tagRepository.GetPostTags(post.Id);
+            List<Tag> allTags = _tagRepository.GetAll();
+
+            PostTagMgmtViewModel vm = new PostTagMgmtViewModel()
+            {
+                Post = post,
+                PostTags = postTags,
+                AllTags = allTags
+            };
+            return View(vm);
+        }
+
+        // POST: PostTagController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostTagController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostTagController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Models/ViewModels/PostTagMgmtViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class PostTagMgmtViewModel
+    {
+        public Post Post { get; set; }
+
+        public List<Tag> PostTags { get; set; }
+
+        public List<Tag> AllTags { get; set; }
+    }
+}
